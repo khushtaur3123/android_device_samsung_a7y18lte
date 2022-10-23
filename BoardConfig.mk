d90766fa6278383cad7723ff3b3cf8711dfbc82d
Khrystan Taur <khushtaur3123@protonmail.com>
2022-10-23 20:04:29 +0530

    Initial Commit 3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,24 +6,29 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_a7y18lte
 
+# TODO:check this
 # Kernel
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := a7y18lte_defconfig
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
+# TODO:check Recovery size
 # Partitions
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3565158400
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4093640704
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
 
+# TODO:complete fstab
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.exynos7885
 
+#TODO:check this
 # Sepolicy
 BOARD_SEPOLICY_TEE_FLAVOR := mobicore
 
 # SPL
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2022-07-01
 
+# TODO:Build this
 # Inherit common board flags
 include device/samsung/universal7885-common/BoardConfigCommon.mk
--- device.mk
+++ device.mk
@@ -1,23 +1,46 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# TODO: Define
+# Init
+PRODUCT_PACKAGES += \
+    init.target.rc
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# System.prop
+TARGET_SYSTEM_PROP += device/samsung/a7y18lte/system.prop
+
+# TODO: Create
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7885-common/universal7885-common.mk)
 
 $(call inherit-product, vendor/samsung/a7y18lte/a7y18lte-vendor.mk)
 
+# TODO: CHeck the need
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@3.0-impl \
     libkeymaster3device
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/android.hardware.keymaster@3.0-service.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/android.hardware.keymaster@3.0-service.xml
-
-# NFC
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
-
+# TODO: Check and revise
 # Rootdir
 PRODUCT_PACKAGES += \
 	fstab.exynos7885 \
